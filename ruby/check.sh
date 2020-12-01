b673147fbc60e8c490dfd6ed1a19bb643b7a2f49
Sean Handley <sean.handley@gmail.com>
2020-12-01 12:47:24 +0000

    Initial commit day 1.

---
+++ ruby/check.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+function test {
+  pushd "day_$1" > /dev/null
+  FILE="output$1.$2.txt"
+  EXPECTED=$(cat $FILE)
+  CMD="./advent$1.$2.rb"
+  ACTUAL=$(cat input.txt | $CMD)
+
+  if [[ $EXPECTED == $ACTUAL ]]
+  then echo "DAY $1.$2 OK"
+  else echo "DAY $1.$2 FAIL"; echo "EXPECTED: \n'$EXPECTED'"; echo "ACTUAL: \n'$ACTUAL'"
+  fi
+
+  popd > /dev/null
+}
+
+test 1 1
+test 1 2
+
+# for i in {1..12}
+# do
+#   for j in {1..2}
+#   do
+#     test $i $j
+#   done
+# done
---
+++ ruby/day_1/advent1.2.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# --- Part Two ---
+# The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+#
+# Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+#
+# In your expense report, what is the product of the three entries that sum to 2020?
+
+require_relative "./advent1.1"
+
+if __FILE__ == $0
+  puts check_report(STDIN.read.split.map(&:to_i), 3, 2020)
+end
