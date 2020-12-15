47eb3bf796e536e476d4ef256965391eb809a01e
Sean Handley <sean.handley@gmail.com>
2020-12-15 21:44:24 +0000

    Refactor.

--- ruby/day_15/advent15.1.rb
+++ ruby/day_15/advent15.1.rb
@@ -1,32 +1,28 @@
 #!/usr/bin/env ruby
 
 @spoken = {}
-@last = 0
-@count = 0
+@last, @count = 0, 0
 
 def input
   @input ||= STDIN.read.split(",").map(&:to_i)
 end
 
 def add_spoken(n)
-  @spoken[n] = [@count, @spoken[n]&.first].compact
-  @last = n
-  @count += 1
-end
-
-def play_round
-  previous_examples = @spoken[@last]
-
-  if previous_examples.count == 1
-    add_spoken(0)
-  else
-    add_spoken(previous_examples.reduce(:-))
+  e = @spoken[n]
+  [@count, e ? e.first : @count].compact.tap do |previous|
+    @spoken[n] = previous
+    @last = n
+    @count += 1
   end
 end
 
+def play_round(previous)
+  add_spoken(previous.reduce(:-))
+end
+
 def play(target)
-  input.each(&method(:add_spoken))
-  (target - @count).times { play_round }
+  previous = input.map(&method(:add_spoken)).last
+  (target - @count).times { previous = play_round(previous) }
   @last
 end
 
